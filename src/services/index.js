4f0df6eb06677bcc13ee8654dfb545cc742d51fe
Paweł Marynowski <yarl@o2.pl>
2016-12-09 22:14:43 +0100

    feat(monument): clean-up view, add image and commons gallery images

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,9 @@
+import commons from './commons.service';
 import theme from './theme.service';
 import wikidata from './wikidata.service';
 
 export default () => {
+  commons();
   theme();
   wikidata();
 };
