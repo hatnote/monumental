346f4b85299830c94526fc8c74e86a0180f17692
Paweł Marynowski <yarl@o2.pl>
2017-03-25 00:59:30 +0100

    feat(services): add lang service

--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,11 @@
+import lang from './lang.service';
 import map from './map.service';
 import theme from './theme.service';
 import wiki from './wiki.service';
 import wikidata from './wikidata.service';
 
 export default () => {
+  lang();
   map();
   theme();
   wiki();
