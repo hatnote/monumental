07a9578078520f637a3a083c551b46353cfa1f83
Paweł Marynowski <yarl@o2.pl>
2017-04-17 18:45:46 +0200

    feat(list): add flag/coat of arms, code rewrite

--- src/components/main/list/list.js
+++ src/components/main/list/list.js
@@ -52,75 +52,84 @@ function controller($state, $stateParams, $timeout, $window, langService, leafle
       vm.place = data[first];
 
       const claims = vm.place.claims;
-      if (vm.place.claims.P18) {
-        claims.P18.values.forEach(image => getImage(image.value));
+      if (vm.place.claims.P41) {
+        claims.P41.values.forEach(image => getImage(image.value));
+      } else if (vm.place.claims.P94) {
+        claims.P94.values.forEach(image => getImage(image.value));
       }
       if (vm.place.claims.P17) {
         const country = claims.P17.values[0];
-        langs = langs.concat(langService.getNativeLanguages(country.value_id));
+        const countryLanguages = langService.getNativeLanguages(country.value_id);
+
+        if (!countryLanguages) { return false; }
+        langs = langs.concat(countryLanguages);
       }
       return true;
     });
   }
 
   function init() {
+    if (!langs) { return; }
     vm.mobile.fullHeader = true;
 
     getPlace()
-    .then(() => {
-      const title = vm.place.labels[vm.lang.code] || vm.place.labels.en || vm.place.id;
-      $window.document.title = `${title} – Monumental`;
-
-      return getList();
-    })
-    .then((data) => {
-      // console.log(data)
-      vm.list = data.map(element => ({
-        name: {
-          value_id: element.item.value.substring(element.item.value.indexOf('/Q') + 1),
-          value: element.itemLabel.value,
-        },
-        admin: {
-          value_id: element.admin.value.substring(element.admin.value.indexOf('/Q') + 1),
-          value: element.adminLabel ? element.adminLabel.value : element.admin.value.substring(element.admin.value.indexOf('/Q') + 1),
-        },
-        coord: element.coord.value ? element.coord.value.replace('Point(', '').replace(')', '').split(' ') : false,
-        image: element.image ? element.image.value.replace('wiki/Special:FilePath', 'w/index.php?title=Special:Redirect/file') + '&width=120' : false
-      }));
-      return vm.list;
-    }).then((list) => {
-      const bounds = [];
-      list.forEach((element) => {
-        if (element.coord) {
-          vm.map.markers[element.name.value_id] = {
-            lat: +element.coord[1],
-            lng: +element.coord[0],
-            message: `
-                <md-list-item class="md-2-line"
-                    ui-sref="main.object({id: ${element.name.value_id.substring(1)}})">
-                <div class="list__image" layout="row" layout-align="center center">
-                  <img ng-src="{{'${element.image}'}}" alt="${element.name.value}" ng-if="${!!element.image}">
-                </div>
-                <div class="md-list-item-text" layout="column">
-                  <p>${element.name.value}</p>
-                  <p class="muted">${element.admin.value}</p>
-                </div>
-              </md-list-item>`,
-            layer: 'monuments',
-            icon,
-          };
-          bounds.push([+element.coord[1], +element.coord[0]]);
-        }
-      });
-      $timeout(() => {
-        vm.showMap = true;
-        leafletData.getMap().then(function (map) {
-          if (bounds.length) {
-            map.fitBounds(bounds, { padding: [25, 25] });
+      .then(() => setTitle())
+      .then(() => getList())
+      .then((data) => {
+        vm.list = data.map(element => ({
+          name: {
+            value_id: element.item.value.substring(element.item.value.indexOf('/Q') + 1),
+            value: element.itemLabel.value,
+          },
+          admin: {
+            value_id: element.admin.value.substring(element.admin.value.indexOf('/Q') + 1),
+            value: element.adminLabel ? element.adminLabel.value : element.admin.value.substring(element.admin.value.indexOf('/Q') + 1),
+          },
+          coord: element.coord.value ? element.coord.value.replace('Point(', '').replace(')', '').split(' ') : false,
+          image: element.image ? element.image.value.replace('wiki/Special:FilePath', 'w/index.php?title=Special:Redirect/file') + '&width=120' : false
+        }));
+        return vm.list;
+      }).then((list) => {
+        const bounds = [];
+        list.forEach((element) => {
+          if (element.coord) {
+            vm.map.markers[element.name.value_id] = {
+              lat: +element.coord[1],
+              lng: +element.coord[0],
+              message: `
+                  <md-list-item class="md-2-line"
+                      ui-sref="main.object({id: ${element.name.value_id.substring(1)}})">
+                  <div class="list__image" layout="row" layout-align="center center">
+                    <img ng-src="{{'${element.image}'}}" alt="${element.name.value}" ng-if="${!!element.image}">
+                  </div>
+                  <div class="md-list-item-text" layout="column">
+                    <p>${element.name.value}</p>
+                    <p class="muted">${element.admin.value}</p>
+                  </div>
+                </md-list-item>`,
+              layer: 'monuments',
+              icon,
+            };
+            bounds.push([+element.coord[1], +element.coord[0]]);
           }
         });
+        $timeout(() => {
+          vm.showMap = true;
+          leafletData.getMap().then(function (map) {
+            if (bounds.length) {
+              map.fitBounds(bounds, { padding: [25, 25] });
+            } else if (vm.place.claims.P625) {
+              const coords = vm.place.claims.P625.values[0].value;
+              vm.map.center = { lat: coords.latitude, lng: coords.longitude, zoom: 10 };
+            }
+          });
+        });
       });
-    });
+  }
+
+  function setTitle() {
+    const title = vm.place.labels[vm.lang.code] || vm.place.labels.en || vm.place.id;
+    $window.document.title = `${title} – Monumental`;
   }
 }
 
