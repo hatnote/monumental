5658f3c1ba7d148525f25d4950ff57efd31bafd8
Paweł Marynowski <yarl@o2.pl>
2017-05-29 23:46:14 +0200

    feat(map): copy functionality from list component

--- src/components/main/map/map.js
+++ src/components/main/map/map.js
@@ -1,3 +1,5 @@
+import L from 'leaflet';
+
 import './map.scss';
 import template from './map.html';
 
@@ -13,45 +15,82 @@ function controller($location, $q, $scope, $state, $stateParams, $timeout, $wind
 
   // bindings
 
-  vm.map = mapService.getMapInstance({ center: { lat: 49.4967, lng: 12.4805, zoom: 4 } });
+  vm.dict = {
+    types: [
+      { label: 'Art', value: '838948' },
+      { label: 'Building', value: '41176' },
+      { label: 'Castle', value: '23413' },
+      { label: 'Cemetery', value: '39614' },
+      { label: 'Manor house', value: '879050' },
+      { label: 'Park', value: '22698' },
+      { label: 'Place of worship', value: '1370598' },
+      { label: 'Residential building', value: '11755880' },
+    ],
+  };
+  vm.filter = angular.extend({ heritage: 1 }, $stateParams);
   vm.list = [];
   vm.listParams = {};
   vm.loading = false;
   vm.loadingMap = true;
+
+  vm.map = mapService.getMapInstance({ center: { lat: 49.4967, lng: 12.4805, zoom: 4 } });
+
+  vm.filterMap = filterMap;
   vm.showMyMap = () => { vm.contentScrolled = true; };
   vm.showMyList = () => { vm.contentScrolled = false; };
+  vm.zoomToID = zoomToID;
 
-  // activate
+  // init
 
-  $scope.$on('centerUrlHash', (event, centerHash) => {
-    $location.search({ c: centerHash });
-  });
-
-  $timeout(() => {
-    vm.loadingMap = false;
-    leafletData.getMap().then((map) => {
-      if (map.getZoom() > 12) {
-        getDataBB(map.getBounds());
-      }
-      map.on('dragend zoomend', () => {
-        if (map.getZoom() > 12) {
-          getDataBB(map.getBounds());
-        }
-      });
-      map.on('dragstart zoomstart', () => {
-        canceler.resolve();
-      });
-    });
-  }, 100);
-
-  $window.document.title = 'Monumental';
+  init();
 
   // functions
+
+  function filterMap() {
+    $state.transitionTo('main.map', vm.filter, { notify: false });
+    leafletData.getMap().then((map) => {
+      if (map.getZoom() > 10) {
+        getDataBB(map.getBounds());
+      }
+    });
+  }
+
+  function init() {
+    $scope.$on('centerUrlHash', (event, centerHash) => {
+      vm.filter.c = centerHash;
+      $state.transitionTo('main.map', vm.filter, { notify: false });
+    });
+
+    $timeout(() => {
+      vm.loadingMap = false;
+      leafletData.getMap().then((map) => {
+        if (map.getZoom() > 10) {
+          getDataBB(map.getBounds());
+        }
+        map.on('dragend zoomend', () => {
+          if (map.getZoom() > 10) {
+            getDataBB(map.getBounds());
+          }
+        });
+        map.on('dragstart zoomstart', () => {
+          canceler.resolve();
+        });
+      });
+    }, 100);
+
+    $window.document.title = 'Monumental';
+  }
 
   function getDataBB(bounds) {
     vm.loading = true;
     canceler.resolve();
     canceler = $q.defer();
+
+    const imageOptions = ['MINUS { ?item wdt:P18 ?image . }', '?item wdt:P18 ?image .'];
+    const image = imageOptions[vm.filter.image] || 'OPTIONAL { ?item wdt:P18 ?image . }';
+
+    const wikipediaOptions = ['FILTER NOT EXISTS { ?article schema:about ?item } .', 'FILTER EXISTS { ?article schema:about ?item } .'];
+    const wikipedia = wikipediaOptions[vm.filter.wikipedia] || '';
 
     request = wikidata.getSPARQL(`SELECT ?item ?itemLabel ?admin ?adminLabel ?image ?coord ?heritage WHERE {
         SERVICE wikibase:box {
@@ -60,22 +99,45 @@ function controller($location, $q, $scope, $state, $stateParams, $timeout, $wind
           bd:serviceParam wikibase:cornerEast "Point(${bounds.getNorthEast().lng} ${bounds.getNorthEast().lat})"^^geo:wktLiteral .
         }
         OPTIONAL { ?item wdt:P131 ?admin . }
-        OPTIONAL { ?item wdt:P18 ?image . }
-        ?item wdt:P1435 ?heritage .
+        ${image}
+        ${getHeritageFilter()}
+        ${vm.filter.type ? `?item wdt:P31 ?type . ?type wdt:P279* wd:Q${vm.filter.type} .` : 'OPTIONAL { ?item wdt:P31 ?type }'}
+        ${wikipedia}
         SERVICE wikibase:label { bd:serviceParam wikibase:language "${langs.map(lang => lang.code).join(',')}" }
       }`, { timeout: canceler.promise });
 
     request.then((data) => {
       vm.map.markers = {};
       // http://stackoverflow.com/a/36744732/1418878
-      vm.list = data
+      const list = data
         .map(element => setListElement(element))
         .filter((element, index, array) => array.findIndex(t => t.name.value_id === element.name.value_id) === index);
+      vm.total = list.length;
+      vm.list = list
+        .slice(0, 2000)
+        .sort((a, b) => (a.name.value > b.name.value) ? 1 : ((b.name.value > a.name.value) ? -1 : 0));
       vm.list.forEach((element) => {
         vm.map.markers[element.name.value_id] = setMarker(element);
       });
+
+      let timeout = null;
+      $scope.$on('leafletDirectiveMarker.mouseover', (event, marker) => { timeout = $timeout(() => { showPopup(event, marker); }, 250); });
+      $scope.$on('leafletDirectiveMarker.mouseout', () => { $timeout.cancel(timeout); });
+      $scope.$on('leafletDirectiveMarker.click', showPopup);
+      vm.loading = false;
+    }).catch(() => {
       vm.loading = false;
     });
+  }
+
+  function getHeritageFilter() {
+    const query = '?item p:P1435 ?monument .';
+    const value = vm.filter.heritage;
+    if (angular.isUndefined(value)) { return `OPTIONAL { ${query} }`; }
+    if (parseInt(value, 10) === 0) { return `MINUS { ${query} }`; }
+    if (parseInt(value, 10) === 1) { return `${query}`; }
+    if (parseInt(value, 10) > 1) { return `?item wdt:P1435 wd:Q${value} .`; }
+    return '';
   }
 
   /**
@@ -88,18 +150,6 @@ function controller($location, $q, $scope, $state, $stateParams, $timeout, $wind
       return `${newImage}&width=120`;
     }
     return false;
-  }
-
-  function getMessage(element) {
-    return `<md-list-item class="md-2-line" ui-sref="main.object({id: ${element.name.value_id.substring(1)}})">
-        <div class="list__image" layout="row" layout-align="center center">
-          <img ng-src="{{ '${element.image}' }}" alt="${element.name.value}" ng-if="${!!element.image}">
-        </div>
-        <div class="md-list-item-text" layout="column">
-          <p>${element.name.value}</p>
-          <p class="muted">${element.admin ? element.admin.value : ''}</p>
-        </div>
-      </md-list-item>`;
   }
 
   function setListElement(element) {
@@ -141,12 +191,59 @@ function controller($location, $q, $scope, $state, $stateParams, $timeout, $wind
 
   function setMarker(element) {
     return {
+      data: element,
       lat: +element.coord[1],
       lng: +element.coord[0],
-      message: getMessage(element),
       layer: 'monuments',
       icon,
     };
+  }
+
+  function showPopup(event, marker) {
+    if (marker.leafletEvent.type === 'click') {
+      const item = vm.list.filter(element => element.name.value_id === marker.model.data.name.value_id)[0];
+      vm.highlighted = item.name.value_id;
+      vm.topIndex = vm.list.indexOf(item);
+    }
+
+    if (marker.leafletObject.getPopup() && marker.leafletObject.isPopupOpen()) {
+      return;
+    }
+    if (marker.leafletObject.getPopup() && !marker.leafletObject.isPopupOpen()) {
+      marker.leafletObject.openPopup();
+      return;
+    }
+
+    const data = marker.model.data;
+    const text = `<md-list-item class="md-2-line"
+                    ui-sref="main.object({id: ${data.name.value_id.substring(1)}})">
+                <div class="list__image" layout="row" layout-align="center center" ng-if="${!!data.image}">
+                  <img ng-src="{{::'${data.image}'}}">
+                </div>
+                <div class="md-list-item-text" layout="column">
+                  <p>${data.name.value}</p>
+                  <p class="muted">${data.admin ? data.admin.value : ''}</p>
+                </div>
+              </md-list-item>`;
+
+    const popup = L.popup({ autoPan: false }).setContent(text);
+    marker.leafletObject.bindPopup(popup);
+    marker.leafletObject.openPopup();
+  }
+
+  function zoomToID(pinId) {
+    const marker = vm.map.markers[pinId];
+    $q.all({
+      map: leafletData.getMap(),
+      markers: leafletData.getMarkers(),
+    }).then((data) => {
+      data.map.setView([marker.lat, marker.lng], 17);
+      showPopup(null, {
+        leafletObject: data.markers[pinId],
+        leafletEvent: {},
+        model: marker,
+      });
+    });
   }
 }
 
