5567da3ebd6ddca49cfce1181d66b3166fbc97e5
Paweł Marynowski <yarl@o2.pl>
2017-07-22 02:24:13 +0200

    feat(monument): add components, clean-up code

---
+++ src/components/main/monument/components/property-quantity.js
@@ -0,0 +1,156 @@
+const PropertyQuantityComponent = {
+  bindings: {
+    claims: '=',
+    edit: '=',
+    labels: '=',
+    lang: '=',
+    property: '=',
+    text: '=',
+  },
+  controller,
+  template: `
+    <span class="muted" flex="35">{{ ::($ctrl.text[$ctrl.property][$ctrl.lang.code] || $ctrl.text[$ctrl.property].en) }}</span>
+    <span flex="65"
+          layout="column" layout-align="start stretch">
+      <span class="monument__details-value"
+              ng-repeat="value in ::$ctrl.propertyClaims">
+        <span ng-if="value.mainsnak.datavalue.value.amount && !$ctrl.edit.all">
+          {{ ::value.mainsnak.datavalue.value.amount }}
+        </span>
+        <span class="muted"
+                ng-if="!value.mainsnak.datavalue.value.amount && !$ctrl.edit.all && $first">
+          unset
+        </span>
+        <div class="property__edit"
+              layout="row" layout-align="start start" 
+              ng-if="$ctrl.edit.all"
+              ng-class="{'property__edit--deleted': value.action.type === 'remove'}">
+          <md-input-container flex
+                    ng-init="value.newValue = value.mainsnak.datavalue.value.amount">
+            <input type="text" aria-label="Value"
+                    ng-model="value.newValue"
+                    ng-change="$ctrl.queueEdit(value, $last)"
+                    ng-disabled="value.action.type === 'remove'">
+          </md-input-container>
+
+          <md-button class="md-primary" aria-label="Category added"
+                      ng-href="//wikidata.org/w/index.php?action=historysubmit&type=revision&diff={{ value.save }}" target="_blank"
+                      ng-if="$ctrl.edit.all && value.save">
+            <md-tooltip md-direction="bottom">Click to see diff or revert edit</md-tooltip>
+            <md-icon>check_circle</md-icon>
+          </md-button>
+          <md-button ng-disabled="value.loading"
+                      ng-click="$ctrl.queueRemove(value)"
+                      ng-if="value.id || value.newValue">
+            <md-progress-circular md-mode="indeterminate" md-diameter="24px" ng-if="value.loading"></md-progress-circular>
+            <md-tooltip md-direction="bottom">Click to remove</md-tooltip>
+            <md-icon ng-if="!value.loading">delete</md-icon>
+          </md-button>
+        </div>
+      </span>
+    </span>`,
+};
+
+function controller($q, $rootScope, $stateParams, wikidata, WikiService) {
+  const vm = this;
+
+  vm.propertyClaims = vm.claims[vm.property];
+
+  vm.queueEdit = queueEdit;
+  vm.queueRemove = queueRemove;
+
+  // init
+
+  if (!vm.propertyClaims) {
+    vm.claims[vm.property] = [{}];
+  } else {
+    vm.claims[vm.property].push({});
+  }
+  vm.propertyClaims = vm.claims[vm.property];
+
+  // functions
+
+  function addClaim(value) {
+    return WikiService.addClaimString({
+      entity: `Q${$stateParams.id}`,
+      property: vm.property,
+      value: angular.toJson({
+        amount: value.newValue,
+        unit: 'http://www.wikidata.org/entity/Q11573',
+      }),
+    });
+  }
+
+  /**
+   * Add edit to queue
+   * @param {Object} value
+   */
+  function queueEdit(value, isLast) {
+    if (value.newValue && isLast) {
+      vm.propertyClaims.push({});
+    }
+
+    value.save = undefined;
+    value.action = {
+      type: 'save',
+      promise: save,
+      value,
+    };
+    $rootScope.$emit('recountQueue');
+  }
+
+  /**
+   * Add remove to queue
+   * @param {Object} value
+   */
+  function queueRemove(value) {
+    if (value.id) {
+      value.action = {
+        type: 'remove',
+        promise: remove,
+        value,
+      };
+    } else {
+      const index = vm.propertyClaims.indexOf(value);
+      if (index !== -1) {
+        vm.propertyClaims.splice(index, 1);
+      }
+    }
+    $rootScope.$emit('recountQueue');
+  }
+
+  function remove(value) {
+    return WikiService.removeClaim({ id: value.id });
+  }
+
+  function save(value) {
+    value.loading = true;
+    const action = value.id ? setClaim : addClaim;
+
+    action(value).then((response) => {
+      value.loading = false;
+      if (response.error) {
+        value.error = response.error.info;
+      } else {
+        value.save = response.data.pageinfo.lastrevid;
+        value.id = response.data.claim.id;
+        value.mainsnak = response.data.claim.mainsnak;
+      }
+    });
+  }
+
+  function setClaim(value) {
+    return WikiService.setClaimQuantity({
+      id: value.id,
+      property: value.mainsnak.property,
+      value: value.newValue,
+      unit: value.mainsnak.datavalue.value.unit,
+    });
+  }
+}
+
+export default () => {
+  angular
+    .module('monumental')
+    .component('moPropertyQuantity', PropertyQuantityComponent);
+};
--- src/components/main/monument/monument.js
+++ src/components/main/monument/monument.js
@@ -7,7 +7,7 @@ import pack from '../../../../package.json';
 
 const MonumentComponent = { controller, template };
 
-function controller($anchorScroll, $http, $mdDialog, $mdMenu, $q, $sce, $stateParams, $timeout, $window, localStorageService, WikiService, imageService, langService, leafletData, mapService, textService, wikidata) {
+function controller($httpParamSerializerJQLike, $anchorScroll, $http, $mdDialog, $mdMenu, $q, $rootScope, $sce, $scope, $stateParams, $timeout, $window, FileUploader, localStorageService, WikiService, imageService, langService, leafletData, mapService, textService, wikidata) {
   const vm = this;
   const icon = mapService.getMapIcon();
   const id = $stateParams.id.includes('Q') ? $stateParams.id : `Q${$stateParams.id}`;
@@ -17,12 +17,14 @@ function controller($anchorScroll, $http, $mdDialog, $mdMenu, $q, $sce, $statePa
   vm.image = [];
   vm.lang = langs[0];
   vm.map = {};
+  vm.queue = [];
   vm.text = null;
 
   vm.getCommonsLink = getCommonsLink;
   vm.getWikipediaArticle = getWikipediaArticle;
   vm.openArticleList = (menu, event) => menu.open(event);
   vm.openImage = openImage;
+  vm.saveAll = saveAll;
   vm.scrollTo = anchor => $anchorScroll(anchor);
   vm.setLabel = setLabel;
 
@@ -108,11 +110,35 @@ function controller($anchorScroll, $http, $mdDialog, $mdMenu, $q, $sce, $statePa
     return value.mainsnak.datavalue.value;
   }
 
+  function saveAll() {
+    const promises = vm.actions.map(promise => promise.promise(promise.value));
+    $q.all(promises).then((data) => {
+      console.log('DONE', data);
+    }).catch((err) => {
+      console.error(err);
+    });
+  }
+
+  function recountQueue() {
+    const claims = _.values(vm.monument.claims);
+    vm.actions = [];
+    claims.forEach((claim) => {
+      claim.forEach((value) => {
+        if (value.action) {
+          vm.actions.push(value.action);
+        }
+      });
+    });
+  }
+
   function init() {
     vm.config = {
       env: $window.__env,
       package: pack,
     };
+
+    const queueListener = $rootScope.$on('recountQueue', () => recountQueue());
+    $scope.$on('$destroy', () => queueListener());
 
     vm.loading = true;
 
@@ -200,5 +226,49 @@ export default () => {
         });
         loadImage();
       },
-    }));
+    }))
+    .directive('ngThumb', ['$window', function ($window) {
+      var helper = {
+        support: !!($window.FileReader && $window.CanvasRenderingContext2D),
+        isFile: function (item) {
+          return angular.isObject(item) && item instanceof $window.File;
+        },
+        isImage: function (file) {
+          var type = '|' + file.type.slice(file.type.lastIndexOf('/') + 1) + '|';
+          return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
+        }
+      };
+
+      return {
+        restrict: 'A',
+        template: '<canvas/>',
+        link: function (scope, element, attributes) {
+          if (!helper.support) return;
+
+          var params = scope.$eval(attributes.ngThumb);
+
+          if (!helper.isFile(params.file)) return;
+          if (!helper.isImage(params.file)) return;
+
+          var canvas = element.find('canvas');
+          var reader = new FileReader();
+
+          reader.onload = onLoadFile;
+          reader.readAsDataURL(params.file);
+
+          function onLoadFile(event) {
+            var img = new Image();
+            img.onload = onLoadImage;
+            img.src = event.target.result;
+          }
+
+          function onLoadImage() {
+            var width = params.width || this.width / this.height * params.height;
+            var height = params.height || this.height / this.width * params.width;
+            canvas.attr({ width: width, height: height });
+            canvas[0].getContext('2d').drawImage(this, 0, 0, width, height);
+          }
+        }
+      };
+    }]);
 };
